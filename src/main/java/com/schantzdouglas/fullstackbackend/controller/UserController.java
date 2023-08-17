2020dcbca0cd3e295756ad87b7e4a085557e3670
douglasschantz <schantzdouglas@gmail.com>
2023-08-17 14:16:40 -0300

    @GetMapping for getting data from database

--- src/main/java/com/schantzdouglas/fullstackbackend/controller/UserController.java
+++ src/main/java/com/schantzdouglas/fullstackbackend/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.schantzdouglas.fullstackbackend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,28 +12,19 @@ import com.schantzdouglas.fullstackbackend.model.User;
 import com.schantzdouglas.fullstackbackend.repository.UserRepository;
 
 @RestController
-//@RequestMapping(value = "/users")
 public class UserController {
 
 	@Autowired
-	private UserRepository userRepository;	
-	
-	/*@GetMapping
-	@CrossOrigin
-	@ResponseStatus(code = HttpStatus.OK)
-	public ResponseEntity<?> listar() {
-		System.out.println("teste");
-		return ResponseEntity.ok().body("lista de usarios");
-	}*/
-	
-	@GetMapping("/users")
-    public String home(){
-        return "Hello World!";
-    }
+	private UserRepository userRepository;
 
 	@PostMapping("/user")
 	User newUser(@RequestBody User newUser) {
 		return userRepository.save(newUser);
+	}
+
+	@GetMapping("/users")
+	List<User> getAllUsers() {
+		return userRepository.findAll();
 	}
 
 }
