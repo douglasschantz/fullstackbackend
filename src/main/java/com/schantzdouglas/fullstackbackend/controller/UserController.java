2b287de803f2af726b06f57a88cf92a4b78446bd
douglasschantz <schantzdouglas@gmail.com>
2023-08-18 11:36:12 -0300

    CrossOrigin

--- src/main/java/com/schantzdouglas/fullstackbackend/controller/UserController.java
+++ src/main/java/com/schantzdouglas/fullstackbackend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.schantzdouglas.fullstackbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.schantzdouglas.fullstackbackend.model.User;
 import com.schantzdouglas.fullstackbackend.repository.UserRepository;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
 	@Autowired
