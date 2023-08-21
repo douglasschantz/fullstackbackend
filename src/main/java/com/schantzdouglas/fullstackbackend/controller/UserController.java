02466029c3ebb0fce3a6f1fb2f46022e65ab0640
douglasschantz <schantzdouglas@gmail.com>
2023-08-21 14:14:52 -0300

    SortById

--- src/main/java/com/schantzdouglas/fullstackbackend/controller/UserController.java
+++ src/main/java/com/schantzdouglas/fullstackbackend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.schantzdouglas.fullstackbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,7 +31,7 @@ public class UserController {
 
 	@GetMapping("/users")
 	List<User> getAllUsers() {
-		return userRepository.findAll();
+		return userRepository.findAll(Sort.by("id"));
 	}
 	
 	@GetMapping("/user/{id}")
